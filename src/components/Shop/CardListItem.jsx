ba28d5f2c41c5d7139451727c0da80d3ac6fbc6d
Zuzana <44017111+zuzanakorinkova@users.noreply.github.com>
2023-12-06 14:03:32 +0100

    build fix

--- src/components/Shop/CardListItem.jsx
+++ src/components/Shop/CardListItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import DummyShoe from '../../assets/dummy_shoe.png'
 import styles from './Shop.module.css';
 
 export default function CardListItem({items}) {
